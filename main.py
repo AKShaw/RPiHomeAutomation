83420099e623f0c3897638437695a86216dff029
Alex Shaw <ashaw1297@gmail.com>
2016-03-22 15:01:59 +0000

    passed in ledScreenObj to the tpl

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def index(area="Home"):
     ledScreenObj["firstLine"] = led.getLine1
     ledScreenObj["secondLine"] = led.getLine2
     
-    return template("www/index.tpl", area=area, weather=getWeatherData(pyowm.OWM("18c319fbdc2695c31d05763b053e1753"), float(config.getLat), float(config.getLong)), config=configObj)
+    return template("www/index.tpl", area=area, weather=getWeatherData(pyowm.OWM("18c319fbdc2695c31d05763b053e1753"), float(config.getLat), float(config.getLong)), config=configObj, led=ledScreenObj
 
 @route("/saveConfig", method="POST")
 def writeConfig():
