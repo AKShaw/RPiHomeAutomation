a4ba427ba0d18c9038898aaf4ecd740db0611564
Alex Shaw <ashaw1297@gmail.com>
2016-03-23 23:01:58 +0000

    Added code for the LCD to work

--- led.py
+++ led.py
@@ -1,14 +1,18 @@
+#from lcd1602 import LCD1602
+
 class SetLED():
     def __init__(self, line1, line2, board):
         self.line1 = line1
         self.line2 = line2
         self.board = board
+        #self.lcd = LCD1602()
         
     @property
     def getLine1(self):
         return self.line1
 
     def setLine1(self, value):
+        #lcd.lcd_string(value, lcd.LCD_LINE_1)
         self.line1 = value
 
     @property
@@ -16,4 +20,5 @@ class SetLED():
         return self.line2
 
     def setLine2(self, value):
+        #lcd.lcd_string(value, lcd.LCD_LINE_2)
         self.line2 = value
