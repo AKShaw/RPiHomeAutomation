50a71c7cc5f927de883e0f0c1e2378d0cf309551
Alex Shaw <ashaw1297@gmail.com>
2016-04-21 16:44:34 +0100

    Modded JS due to 500 server error (Changed method to return into event listener)

--- www/tpl/js/lighting.js
+++ www/tpl/js/lighting.js
@@ -1,16 +1,15 @@
-
-var status=1;
-
 function updateValue(){
+	var status = 1;	
 	if (toggleBtn.checked()==true){
-		status=1;
+		status="1";
 	}
 	else if (toggleBtn.checked()==false){
-		status=0;
+		status="0";
 	}
+	return status;
 }
 
 form.addEventListener('iron-form-presubmit', function() {
 	//this.request.method = 'POST';
-	this.request.params["onOff"] = status;
+	this.request.params["onOff"] = updateValue();
 });
