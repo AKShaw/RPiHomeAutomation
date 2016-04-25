1b78e2fe890f29103ab94df3fe3f63475f8693d6
Alex Shaw <ashaw1297@gmail.com>
2016-04-25 12:59:23 +0100

    Added PIR and buzzer

--- pir_buzzer.py
+++ pir_buzzer.py
@@ -1,8 +1,8 @@
 class PirBuzzer:
     def __init__(self, board, pir_pin, buzz_pin):
         self.board = board
-        board.GPIO.setup(pir_pin, GPIO.IN)
-        board.GPIO.setup(buzz_pin, GPIO.OUT)
+        board.GPIO.setup(pir_pin, board,GPIO.IN)
+        board.GPIO.setup(buzz_pin, board,GPIO.OUT)
         board.GPIO.add_event_detect(pir_pin, board.GPIO.FALLING, callback=buzz, bouncetime=300)
 
     def detectMotion(self):
@@ -14,7 +14,7 @@ class PirBuzzer:
             time.sleep(1)
 
     def buzz(self):
-        board.GPIO.output(BUZZ_PIN, GPIO.HIGH)
+        board.GPIO.output(self.buzz_pin, board.GPIO.HIGH)
         time.sleep(0.1)
-        board.GPIO.output(BUZZ_PIN, GPIO.LOW)
+        board.GPIO.output(self.buzz_pin, board.GPIO.LOW)
         
