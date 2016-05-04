9fbd31a4d4fe81f25e0de43d4ed64b58bb9133fb
Alex Shaw <ashaw1297@gmail.com>
2016-05-04 12:50:00 +0100

    Trying different camera code

--- www/tpl/js/camera.js
+++ www/tpl/js/camera.js
@@ -1,6 +1,6 @@
 setInterval(function() {
 	var image = document.createElement("img");
-	var card = document.getElementByClassName("camContain");
+	var card = document.getElementsByClassName("camContain");
 	image.className = "camera";
 	image.id = "frame";
 	image.src= "/video_feed";
