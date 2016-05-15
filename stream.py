aea11079e70dcf834adcd18e5870826bf33b606e
Alex Shaw <ashaw1297@gmail.com>
2016-05-15 16:26:34 +0100

    I dont even know how to describe what im doing

--- stream.py
+++ stream.py
@@ -6,11 +6,11 @@ class Stream():
 
     def getData(self):
         with picamera.PiCamera() as camera:
-            with IM()as im:
-                camera.resolution=(640,480)
-                camera.capture(im, format="jpeg")
-                print("Printing image file...")
-                return im.read()
+            im = IM()
+            camera.resolution=(640,480)
+            camera.capture(im, format="jpeg")
+            print("Printing image file...")
+            return im.read()
 
 class IM:
     def __init__(self):
