286cdaad6a9de254d0345f860ddb6bf67e9d9b71
Alex Shaw <ashaw1297@gmail.com>
2016-05-15 16:12:42 +0100

    Changed im variable to a file object

--- stream.py
+++ stream.py
@@ -7,8 +7,9 @@ class Stream():
     def getData(self):
         with picamera.PiCamera() as camera:
             with open("image", "w+b") as im:
-                camera.resolution=(640,480)
-                camera.capture(im, format="jpeg")
+                #camera.resolution=(640,480)
+                #camera.capture(im, format="jpeg")
+                im.write("test")
                 print("Printing image file...")
                 print (im.read())
                 return im.read()
