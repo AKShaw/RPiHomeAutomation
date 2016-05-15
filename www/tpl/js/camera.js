55ebf98037fa338ca7ba77aafab340b16b9555da
Alex Shaw <ashaw1297@gmail.com>
2016-05-15 16:59:04 +0100

    I dont even know how to describe what im doing

--- www/tpl/js/camera.js
+++ www/tpl/js/camera.js
@@ -1,9 +1,8 @@
 $( document ).ready(function() {
 	var camContain = $(".camContain");
-	var i = 0;
 	setInterval(function() {
 		$(".camera").remove();
-		camContain.append("<img class='camera' src='/camFeed?i="+i.toString()+"'>");
-		i = i+1;
+		var time = Date.now() / 1000 | 0;
+		camContain.append("<img class='camera' src='/camFeed?i="+time+"'>");
 	}, 1000);
 });
